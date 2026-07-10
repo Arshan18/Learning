fc6d840f9567ddbdba5851165eddef4ace15644c
Arshan18 <arshanattar09@gmail.com>
2026-07-10 20:06:01 +0530

    Learning

---
+++ Chp-4/practice.py
@@ -0,0 +1,56 @@
+# Problem1: Write a python program to store seven marks in a list entered by the user.
+
+# marks = []
+
+# f1 = int(input("Enter marks here: "))
+# marks.append(f1)
+# f2 = int(input("Enter marks here: "))
+# marks.append(f2)
+# f3 = int(input("Enter marks here: "))
+# marks.append(f3)
+# f4 = int(input("Enter marks here: "))
+# marks.append(f4)
+# f5 = int(input("Enter marks here: "))
+# marks.append(f5)
+# f6 = int(input("Enter marks here: "))
+# marks.append(f6)
+# f7 = int(input("Enter marks here: "))
+# marks.append(f7)
+
+# print(marks)
+# print(marks.sort())
+
+# Problem2: Write a python program to accept marks of 6 subjects and display them in a sorted manner.
+
+# marks = []
+
+# f1 = int(input("Enter marks here: "))
+# marks.append(f1)
+# f2 = int(input("Enter marks here: "))
+# marks.append(f2)
+# f3 = int(input("Enter marks here: "))
+# marks.append(f3)
+# f4 = int(input("Enter marks here: "))
+# marks.append(f4)
+# f5 = int(input("Enter marks here: "))
+# marks.append(f5)
+# f6 = int(input("Enter marks here: "))
+# marks.append(f6)
+
+# marks.sort()
+# print(marks)
+
+# Problem3: Check that a tuple type cannot be changed in python. 
+
+# a = (1, 2, 3, 4, 5)
+# a[0] = 10
+
+# Problem4: Write to program to sum a list with 4 numbers.
+
+# l = [1, 3, 5, 7]
+# print(sum(l))
+
+# Problem5: Write a program to count the number of zeros in the following tuple.
+a = (7, 0, 8, 0, 0, 9)
+
+print(a.count(0))
