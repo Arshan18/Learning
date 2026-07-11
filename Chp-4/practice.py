1999707b43c54000e5655e0cf1194acf5905409d
Arshan18 <arshanattar09@gmail.com>
2026-07-11 17:13:28 +0530

    Learning

--- Chp-4/practice.py
+++ Chp-4/practice.py
@@ -51,6 +51,9 @@
 # print(sum(l))
 
 # Problem5: Write a program to count the number of zeros in the following tuple.
-a = (7, 0, 8, 0, 0, 9)
 
+b = (2000, 1000, 3000, 2304)
+print(b.count(0))
+
+a = (7, 0, 8, 0, 0, 9)
 print(a.count(0))
