f89b355fc2047bf78493da92cebf0e188441842c
Arshan18 <arshanattar09@gmail.com>
2026-07-12 16:26:10 +0530

    Learning

---
+++ dict.py
@@ -0,0 +1,13 @@
+Marks = {
+    "Andrew": 96 ,
+    "Senorita": 69,
+    "Sam": 56,
+    "list": [1, 2, 3, 4]
+}
+
+print(Marks)
+print(type(Marks))
+
+print(Marks.items())
+print(Marks.keys())
+print(Marks.values())
