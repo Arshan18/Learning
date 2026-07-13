4632ce7560ab87505fde3d23169f0bd88c042af6
Arshan18 <arshanattar09@gmail.com>
2026-07-13 17:04:15 +0530

    Learning

--- Chp-5/practice.py
+++ Chp-5/practice.py
@@ -1,4 +1,4 @@
-# Write a program to create a dictionary of Hindi Words with values as their English translation. Provide a userwith an option to look it up!
+# Problem1: Write a program to create a dictionary of Hindi Words with values as their English translation. Provide a userwith an option to look it up!
 
 Words = {
     "Billi": "Cat",
@@ -8,6 +8,36 @@ Words = {
     "Ghar": "House"
 }
 
-word = input("Enter a Hindi word:")
+# word = input("Enter a Hindi word:")
 
-print("Meaning:", Words.get(word, "Word not found in dictionary"))
+# print("Meaning:", Words.get(word, "Word not found in dictionary")) 
+
+# Problem2: Write a program to input 8 mumber from the user and display all the unique number once
+
+s = set()
+
+# n1 = input("Enter no 1:")
+# s.add(int(n1))
+# n2 = input("Enter no 2:")
+# s.add(int(n2))
+# n3 = input("Enter no 3:")
+# s.add(int(n3))
+# n4 = input("Enter no 4:")
+# s.add(int(n4))
+# n5 = input("Enter no 5:")
+# s.add(int(n5))
+# n6 = input("Enter no 6:")
+# s.add(int(n6))
+# n7 = input("Enter no 7:")
+# s.add(int(n7))
+# n8 = input("Enter no 8:")
+# s.add(int(n8))
+
+# print(s)
+
+# Problem3: can we have set with 18 (int) and '18' stras a value in it 
+
+s1 = set()
+s1.add(18)
+s1.add("18")
+print(s1)
