d044a701b7b66238ee70db53251896981a1fa6dc
Arshan18 <141809406+Arshan18@users.noreply.github.com>
2026-08-03 19:40:18 +0530

    Learning

---
+++ Chp-7/while_loop.py
@@ -0,0 +1,15 @@
+l = [1, "Samay", 18, "Rohit", 45, "Virat", 100]
+
+i = 0 
+
+while(i < len(l)):
+      print(l[i])
+      i += 1
+
+
+# Print Sammy 5 times using while loop
+
+i = 0 
+while(i < 5):
+      print("Sammy")
+      i += 1
